aa2634eabe9550e2c0339779724110d52f0411fd
carranj <carranco.jose.r@gmail.com>
2021-09-25 17:48:38 -0500

    Initial React + Laravel project frameworks

---
+++ resources/js/index.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import {render} from "react-dom";
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+             
+        </div>
+    )
+};
+
+render(<App />, document.getElementById('app'));
