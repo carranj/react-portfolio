1593a7bddbfef696a4e824ab66cfd1281edf73b0
Jose Carranco <jcarranco@tvi-mp3.com>
2021-10-20 15:36:45 -0500

    mail test

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -11,9 +11,20 @@ class ContactController extends Controller
     public function contactSubmit(Request $request)
     {
         $data = request()->input('data');
+        $to_name = $data['fName'] . ' ' . $data['lName'];
+        $to_email = $data['email'];
 
-        Mail::to('carranco.jose.r@gmail.com')->send(new ContactMail($data));
+        Mail::send('emails.mail', $data, function ($message) use ($to_name, $to_email) {
+            $message->to('carranco.jose.r@gmail.com', 'Jose Carranco')
+                ->subject('Portfolio Contact form filled');
 
-        return response(["message_sent" => 'message sent'], 200);;
+            $message->from($to_email, $to_name);
+        });
+
+
+
+        // Mail::to('carranco.jose.r@gmail.com')->send(new ContactMail($data));
+
+        return response(["message_sent" => 'message sent'], 200);
     }
 }
