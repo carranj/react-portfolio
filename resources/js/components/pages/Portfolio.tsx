201705bca0efc1544f2bcbacfed2e7f2f1c06427
carranj <carranco.jose.r@gmail.com>
2021-11-29 20:35:12 -0600

    style updates and remove passcode

--- resources/js/components/pages/Portfolio.tsx
+++ resources/js/components/pages/Portfolio.tsx
@@ -70,19 +70,9 @@ export const Portfolio = () => {
 
         <div className="portfoliolists">
           <div className="row mb-3 categoryFilters">
+            
             <div className="col-md-4">
-              <p
-                style={{
-                  paddingTop:10,
-                  paddingBottom:0,
-                  paddingLeft:15,
-                  paddingRight:15
-                }}>
-                  Filter projects by skill{isAuthorized === 0 && <span> or enter code to see all projects</span>}
-              </p>
-            </div>
-            <div className="col-md-4">
-            <FormControl sx={{ m: 1 }} fullWidth={true}>
+            <FormControl fullWidth={true}>
                 <InputLabel id="select-skills">Skills</InputLabel>
                 <Select
                   labelId="select-skills"
@@ -112,30 +102,7 @@ export const Portfolio = () => {
                 </Select>
               </FormControl>
             </div>
-            <div className="col-md-4">
-              {(isAuthorized === 0 )&& (
-                <>
-                  <FormControl sx={{ m: 1 }} fullWidth={true}>
-                    <TextField
-                      id="authorize"
-                      label="Enter Passcode"
-                      variant="outlined"
-                      onChange={(e) => setAuthCode(e.target.value)}
-                      style={{
-                        backgroundColor:'white'
-                      }}
-                      InputProps={{endAdornment: <Button variant="contained" onClick={handleAuth} >Authorize</Button>}} />
-                  </FormControl>
-                </>
-              )}
-              
-              {(isAuthorized === 1) &&(
-                <>
-                <p>Authorized</p>
-                </>
-              )}
             
-            </div>
           </div>
           
           {
