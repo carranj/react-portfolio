1d309372632bda4135d33f68321a32d7ca6b516c
Jose Carranco <carranco.jose.r@gmail.com>
2026-01-18 03:25:33 -0600

    add clarity design ui

--- resources/angular/src/app/app.module.ts
+++ resources/angular/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ClarityModule } from '@clr/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,8 +22,10 @@ import { PortfolioComponent } from './pages/portfolio/portfolio.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    ClarityModule,
     AppRoutingModule
   ],
   providers: [],
